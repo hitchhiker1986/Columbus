3a3f0ddad63585e4bc94bb0c67169a872c5d3758
hitchhiker1986 <papp.l@icloud.com>
2024-06-02 19:06:05 +0200

    minor modifications

--- Columbus/columbus/admin.py
+++ Columbus/columbus/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Apartment, Owner, Tenant, Utility, Task
 # Register your models here.
+
+admin.site.register(Apartment)
+admin.site.register(Owner)
+admin.site.register(Tenant)
+admin.site.register(Utility)
+admin.site.register(Task)
